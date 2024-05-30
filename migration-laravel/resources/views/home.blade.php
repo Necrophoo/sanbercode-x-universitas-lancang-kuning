69fe05ef1faee428abd12437669275e3811992ca
attallahrasyidalfath <rasyidalfath3@gmail.com>
2024-05-30 22:43:00 +0700

    mengumpulkan tugas laravel-migration

---
+++ migration-laravel/resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <div>
+        <div class="">
+          <h1>SANBERBOOK</h1>
+          <p><b>Social Media Developer berkualitas</b></p>
+          <p>Belajar dan berbagi agar hidup semakin berkualitas</p>
+          <h2>BENEFIT JOIN SANBERBOOK</h2>
+        </div>
+  
+        <div class="">
+          <ul>
+            <li>Mendapatkan motivasi dari Developer</li>
+            <li>Sharing knowledge dari para sanber</li>
+            <li>dibuat oleh web Developer terbaik</li>
+            <li>berkualitas</li>
+          </ul>
+  
+          <h2>Cara Bergabung</h2>
+          <ol>
+            <li>Kunjungi website ini</li>
+            <li>mendaftar di <a href="{{ route('register') }}">Form Sign Up</a></li>
+            <li>selesai</li>
+          </ol>
+        </div>
+      </div>
+</body>
+</html>
